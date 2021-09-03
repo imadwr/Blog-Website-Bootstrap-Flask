2d81002a31198e31c324282732eaf29e79c0a3b4
Imad <imadouairem96@gmail.com>
2021-09-03 18:22:25 +0100

    Send email from contact page form

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 posts = requests.get(url="https://api.npoint.io/a9f67db60d74f9a52874").json()
+EMAIL = os.getenv("email")
+PASSWORD = os.getenv("password")
+to_address = os.getenv("address")
 
 app = Flask(__name__)
 
@@ -16,9 +21,26 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        message_to_send = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nmessage: {message}"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=to_address,
+                msg=f"Subject: New Message From Blog\n\n{message_to_send}"
+            )
+        return render_template("contact.html", msg_sent=True)
+    else:
+        return render_template("contact.html", msg_sent=False)
 
 
 @app.route("/post/<int:index>")
